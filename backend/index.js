5c6bc7e73d3ede0da24bf68c3456cebdf8672dec
Juan Francisco Siaba <jfsiaba@mail.austral.edu.ar>
2026-03-17 10:05:38 -0300

    messi

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Backend funcionando 🚀");
+    res.send("Backend funcionando ");
 });
 
 app.listen(3000, () => {
