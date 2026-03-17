c36a852d7c6d78409909f6e34a06bdd52beb3f76
Juan Francisco Siaba <jfsiaba@mail.austral.edu.ar>
2026-03-17 09:59:12 -0300

    hola

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Backend funcionando 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
