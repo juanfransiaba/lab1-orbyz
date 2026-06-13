e3ef2ff7d6ab5e0b023c550ac5dbde39b9971024
JuanRudolph <jrudolph@mail.austral.edu.ar>
2026-06-13 17:22:07 -0300

    cambios

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ function isAllowedLocalOrigin(origin) {
         return true;
     }
 
-    return /^http:\/\/(localhost|127\.0\.0\.1):\d+$/.test(origin);
+    return /^http:\/\/(localhost|127\.0\.0\.1|192\.168\.\d{1,3}\.\d{1,3}|10\.\d{1,3}\.\d{1,3}\.\d{1,3}|172\.(1[6-9]|2\d|3[01])\.\d{1,3}\.\d{1,3}):\d+$/.test(origin);
 }
 
 app.use(
--- backend/socket/index.js
+++ backend/socket/index.js
@@ -12,7 +12,8 @@ function isAllowedLocalOrigin(origin) {
     if (!origin) {
         return true;
     }
-    return /^http:\/\/(localhost|127\.0\.0\.1):\d+$/.test(origin);
+
+    return /^http:\/\/(localhost|127\.0\.0\.1|192\.168\.\d{1,3}\.\d{1,3}|10\.\d{1,3}\.\d{1,3}\.\d{1,3}|172\.(1[6-9]|2\d|3[01])\.\d{1,3}\.\d{1,3}):\d+$/.test(origin);
 }
 
 function initSocket(server) {
